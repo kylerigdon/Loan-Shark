0a80f274a6a60a05e78445f436e8d7d969af5672
Kyle Rigdon <krigdon64@gmail.com>
2024-04-16 18:54:25 -0500

    cleaned up javascript code made to display the values calculated by the users inputs.

--- js/site.js
+++ js/site.js
@@ -1,96 +1,119 @@
 // get the Values from the user
 function getValues() {
   // get the input for loanNumber
-  let balance = document.getElementById('loanNumber').value;
+  let balance = document.getElementById("loanNumber").value;
   // get the input for termNumber
-  let term = document.getElementById('termNumber').value;
+  let term = document.getElementById("termNumber").value;
   // get the input for interestNumber
-  let rate = document.getElementById('interestNumber').value;
+  let rate = document.getElementById("interestNumber").value;
   // convert the input strings into numbers
-
 
   balance = Number(balance);
   term = Number(term);
   rate = Number(rate);
 
-  if (isNaN(balance) || isNaN(term) || isNaN(rate) || balance < 0 || term < 1 || rate < 1 || rate > 100) {
+  if (
+    isNaN(balance) ||
+    isNaN(term) ||
+    isNaN(rate) ||
+    balance < 0 ||
+    term < 1 ||
+    rate < 1 ||
+    rate > 100
+  ) {
     Swal.fire({
       icon: "error",
       title: "Oops!",
-      text: 'Please enter a valid number.',
-      backdrop: false
+      text: "Please enter a valid number.",
+      backdrop: false,
     });
   } else {
-    // calculate total monthly payment
-  const totalMonthlyPayment = ((balance)*(rate/1200)/(1-(1 + rate/1200)**(- term))).toFixed(2);
-  // calculate remaining balance
-  let values = [];
-  let previousRemainingBalanceArray = [];
-  let termMonths = [];
-  let interest = [];
-  let principalArray = [];
-  let previousRemainingBalance = '';
-  let principal = '';
-  
-  for(let months = 1; months <= term; months++) {
-    termMonths.push(months);
-  }
+    let calculatedValues = calculateValues(balance, term, rate);
 
-  for (let index = 0; index < termMonths.length; index++) {
-    let month = termMonths[index];
-    if (month == 1){
-      let remainingBalance = balance;
-      previousRemainingBalance = remainingBalance;
-      values.push(previousRemainingBalance);
-      let interestPayment = (previousRemainingBalance*rate/1200).toFixed(2);
-      values.push(interestPayment);
-      let principalPayment = (totalMonthlyPayment - interestPayment).toFixed(2);
-      principal = principalPayment;
-      values.push(principal);
-    } else {
-      remainingBalance = (previousRemainingBalance-principal).toFixed(2);
-      previousRemainingBalance = remainingBalance;
-      values.push(previousRemainingBalance);
-      let interestPayment = (previousRemainingBalance*rate/1200).toFixed(2);
-      values.push(interestPayment);
-      let principalPayment = (totalMonthlyPayment - interestPayment).toFixed(2);
-      principal = principalPayment;
-      values.push(principal);
-    }
-  }
-  displayValues(termMonth, values);
+    displayValues(calculatedValues, balance);
   }
 }
 
+function calculateValues(balance, term, rate) {
+  // calculate total monthly payment
+  const totalMonthlyPayment =
+    (balance * (rate / 1200)) / (1 - (1 + rate / 1200) ** -term);
+  // calculate remaining balance
+  let termMonths = [];
+  let previousRemainingBalanceArray = [];
+  let interest = [];
+  let principalArray = [];
+  let totalInterestArr = [];
+  let previousRemainingBalance = balance;
+  let totalInterest = 0;
 
-// function displayValues(values) {
+  for (let months = 1; months <= term; months++) {
+    termMonths.push(months);
 
-//   let tableHtml = '<tr>';
+    let interestPayment = (previousRemainingBalance * rate) / 1200;
+    interest.push(interestPayment);
+    totalInterest += interestPayment;
+    totalInterestArr.push(totalInterest);
 
-//   let j = 0;
+    let principalPayment = totalMonthlyPayment - interestPayment;
+    principalArray.push(principalPayment);
 
-//   for (let i = 0; i < values.length; i++) {
-//     let value = values[i];
+    previousRemainingBalance -= principalPayment;
+    previousRemainingBalanceArray.push(previousRemainingBalance);
+  }
 
-//     tableHtml += `<td>${value}</td>`
+  // create an object to hold the results
+  let results = {
+    monthsArr: termMonths,
+    paymentNum: totalMonthlyPayment,
+    principalArr: principalArray,
+    interestArr: interest,
+    totalInterestArr: totalInterestArr,
+    totalInterestNum: totalInterest,
+    balanceArr: previousRemainingBalanceArray,
+  };
 
+  return results;
+}
 
-//   }
-//   if (j % 6 == 0) {
-//     tableHtml += '</tr><tr>'
-//   }
+// display the results here in a table
+function displayValues(myObject, loanAmount) {
+  let totalCost = (loanAmount + myObject.totalInterestNum).toFixed(2);
+  let totalPrincipal = myObject.totalInterestNum.toFixed(2);
+  let loan = loanAmount.toFixed(2);
 
-// let tbody = document.getElementById('tableContent');
-// tbody.innerHTML = tableHtml;
+  let usDollar = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
-// }
+  let summary = document.getElementById("summary");
+  let summaryContent = summary.content;
 
+  let principalElement = summary.querySelector(".totalPrincipal");
+  principalElement.textContent = usDollar.format(loan);
+  let interestElement = summary.querySelector(".totalInterest");
+  interestElement.textContent = usDollar.format(
+    myObject.totalInterestNum.toFixed(2)
+  );
+  let costElement = summary.querySelector(".totalCost");
+  costElement.textContent = usDollar.format(totalCost);
 
-// let tableHtml = '<tr>'
-//let j = 1;
+  tableHtml = "";
 
-// if (j % 6 == 0) {
-//   tableHtml += '</tr><tr>'
-// }
+  for (let i = 0; i < myObject.monthsArr.length; i++) {
+    let month = myObject.monthsArr[i];
+    let principal = myObject.principalArr[i].toFixed(2);
+    let interest = myObject.interestArr[i].toFixed(2);
+    let totalInterests = myObject.totalInterestArr[i].toFixed(2);
+    let balance = myObject.balanceArr[i].toFixed(2);
+    let totalInterest = myObject.totalInterestNum.toFixed(2);
+    let payment = myObject.paymentNum.toFixed(2);
 
-// j++
+    tableHtml += `<tr>
+    <td>${month}</td><td>${payment}</td><td>${principal}</td><td>${interest}</td><td>${totalInterests}</td><td>${balance}</td></tr>`;
+  }
+
+  let tbody = document.getElementById("tableContent");
+  tbody.innerHTML = tableHtml;
+}
