7f25408cc3213bc43e69dbc02c30a523c904e19c
Kyle Rigdon <krigdon64@gmail.com>
2024-04-17 11:00:41 -0500

    updated the sudo-code

--- js/site.js
+++ js/site.js
@@ -6,12 +6,13 @@ function getValues() {
   let term = document.getElementById("termNumber").value;
   // get the input for interestNumber
   let rate = document.getElementById("interestNumber").value;
-  // convert the input strings into numbers
 
+  // convert the input strings into numbers
   balance = Number(balance);
   term = Number(term);
   rate = Number(rate);
 
+  // check to make sure the user input a number
   if (
     isNaN(balance) ||
     isNaN(term) ||
@@ -28,8 +29,10 @@ function getValues() {
       backdrop: false,
     });
   } else {
+    //send the values to calculate our information
     let calculatedValues = calculateValues(balance, term, rate);
 
+    // pass the balance and calculatedValues to be used to display their values
     displayValues(calculatedValues, balance);
   }
 }
@@ -38,26 +41,31 @@ function calculateValues(balance, term, rate) {
   // calculate total monthly payment
   const totalMonthlyPayment =
     (balance * (rate / 1200)) / (1 - (1 + rate / 1200) ** -term);
-  // calculate remaining balance
+
   let termMonths = [];
   let previousRemainingBalanceArray = [];
   let interest = [];
   let principalArray = [];
   let totalInterestArr = [];
+  // set previousRemainingBalance to initially be the value of balance
   let previousRemainingBalance = balance;
   let totalInterest = 0;
 
   for (let months = 1; months <= term; months++) {
     termMonths.push(months);
 
+    // calculate the interest payment and add it to the interest array
     let interestPayment = (previousRemainingBalance * rate) / 1200;
     interest.push(interestPayment);
+    // calculate the total interest paid for each month and add it to totalInterestArr
     totalInterest += interestPayment;
     totalInterestArr.push(totalInterest);
 
+    // calculate the principal payments and add each iteration to the principalArray
     let principalPayment = totalMonthlyPayment - interestPayment;
     principalArray.push(principalPayment);
 
+    // calculate remaining balance and add each iteration to the previousRemainingBalanceArray
     previousRemainingBalance -= principalPayment;
     previousRemainingBalanceArray.push(previousRemainingBalance);
   }
@@ -82,16 +90,22 @@ function displayValues(myObject, loanAmount) {
   let totalPrincipal = myObject.totalInterestNum.toFixed(2);
   let loan = loanAmount.toFixed(2);
 
+  // configure a formatter to USD
   let usDollar = Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   });
 
-  document.getElementById('monthlyPayment').textContent = usDollar.format(myObject.paymentNum);
+  // getting the element with an id of monthlyPayment and changing the text to be the value of paymentNum
+  document.getElementById("monthlyPayment").textContent = usDollar.format(
+    myObject.paymentNum
+  );
 
+  // getting the element with an id of summary and changing the content of it
   let summary = document.getElementById("summary");
   let summaryContent = summary.content;
 
+  // targetting the classes of the elements I would like to change
   let principalElement = summary.querySelector(".totalPrincipal");
   principalElement.textContent = usDollar.format(loan);
   let interestElement = summary.querySelector(".totalInterest");
@@ -101,8 +115,12 @@ function displayValues(myObject, loanAmount) {
   let costElement = summary.querySelector(".totalCost");
   costElement.textContent = usDollar.format(totalCost);
 
+  // setting the html table content to be empty
   tableHtml = "";
 
+  // using a for loop to search through myObject and get the monthsArr length and
+  // increment through it while also declaring each variable as one of the corrisponding arrays or numbers
+  // in the object then creating a table using them.
   for (let i = 0; i < myObject.monthsArr.length; i++) {
     let month = myObject.monthsArr[i];
     let principal = myObject.principalArr[i].toFixed(2);
@@ -116,6 +134,7 @@ function displayValues(myObject, loanAmount) {
     <td>${month}</td><td>${payment}</td><td>${principal}</td><td>${interest}</td><td>${totalInterests}</td><td>${balance}</td></tr>`;
   }
 
+  // displaying the created table on the html page
   let tbody = document.getElementById("tableContent");
   tbody.innerHTML = tableHtml;
 }
